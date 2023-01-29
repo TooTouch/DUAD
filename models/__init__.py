a8219b4b7287a3eac536d3e627b8873af80a8bf7
SunwooKimstar <emilyswkim@gmail.com>
2023-01-29 14:29:47 +0900

    [MODIFY] files

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,6 @@
 from .ae import AutoEncoder
 from .duad import DUAD
 from .factory import create_model
+from .simple_ae import AutoEncoder
+from .duad_KDDCUP import DUAD_KDDCUP
+from .factory import create_kddcup_model
